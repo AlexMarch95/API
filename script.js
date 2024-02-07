95c8c2e13e276f6839b24a4b9370b2615b13fe0b
AlexMarch95 <bigbenfaq@mail.ru>
2024-02-07 23:03:34 +0300

    feat: creat function getData() and load favicon, also creat user interface

---
+++ script.js
@@ -0,0 +1,10 @@
+const url = "https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo"
+async function getData() {
+    const res = await fetch(url);
+    const data = await res.json();
+    const dataPhoto = data.urls.regular
+    console.log(dataPhoto);
+  }
+
+  
+  getData();
